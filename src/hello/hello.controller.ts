6edda25aec359c79d98f47341f94896a988e8c98
Facundo Ríos <Facundorios2005@gmail.com>
2024-05-26 03:24:10 -0300

    feat: added http status error

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -1,13 +1,33 @@
-import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Controller, Get, HttpCode, Req, Res } from '@nestjs/common';
 import { Request, Response } from 'express';
 
 @Controller()
 export class HelloController {
-  @Get('/poo')
+  @Get('/hello')
   index(@Req() request: Request, @Res() response: Response) {
     console.log(request.url);
     response.status(200).json({
       message: 'Hello World',
     });
   }
+  @Get('new')
+  @HttpCode(201)
+  new() {
+    return 'This action adds a new cat';
+  }
+
+  @Get('notfound')
+  @HttpCode(404)
+  notFound() {
+    return 'This action returns a not found response';
+  }
+
+  @Get('error')
+  @HttpCode(500)
+  error() {
+    return 'This action returns an error response';
+  }
+  
+
+
 }
